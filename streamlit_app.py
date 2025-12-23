a32b578a1f984eedc4017210922c9bb84a0801ea
Gerard SoftServian <garev@softserveinc.com>
2025-12-23 17:17:39 -0500

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session \
     .table("smoothies.public.fruit_options") \
-    .select(col("FRUIT_NAME"))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+    .select(col("FRUIT_NAME"),col("SEARCH_ON"))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingridients_list = st.multiselect(
     "Choose up to 5 ingridients"
